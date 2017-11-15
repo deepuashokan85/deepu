bffe5906eadfba8827460d224cfa6f8bf125614d
root <deepashokan85@gmail.com>
2017-11-16 01:16:20 +0530

    added

--- aws-ec2-launch.sh
+++ aws-ec2-launch.sh
@@ -1,1 +1,3 @@
+echo "creating ec2 instance"
 aws ec2 run-instances --image-id ami-c998b6b2 --count 1 --instance-type t2.micro --key-name pradeep --security-group-ids sg-bb6718c6
+echo "EC2 instance launched"
