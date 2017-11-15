5c0f156b0f1b01ce29c0c0b9df7d23dd5169b076
root <deepashokan85@gmail.com>
2017-11-16 01:34:39 +0530

    http

--- aws-ec2-launch.sh
+++ aws-ec2-launch.sh
@@ -1,3 +1,3 @@
 echo "creating ec2 instance"
-aws ec2 run-instances --image-id ami-c998b6b2 --count 1 --instance-type t2.micro --key-name pradeep --security-group-ids sg-bb6718c6
+aws ec2 run-instances --image-id ami-c998b6b2 --count 1 --instance-type t2.micro --key-name pradeep --user-data file://http_script.txt --security-group-ids sg-bb6718c6
 echo "EC2 instance launched"
