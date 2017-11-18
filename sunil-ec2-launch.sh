e02d82a90942bb2c797cae6346bad095cb1fea58
root <deepashokan85@gmail.com>
2017-11-18 11:31:04 +0530

    launch ec2

---
+++ sunil-ec2-launch.sh
@@ -0,0 +1,5 @@
+echo "creating the ec2 instance"
+aws ec2 run-instances --image-id ami-c998b6b2 --count 1 --instance-type t2.micro --key-name pradeep --user-data file://http_script.txt --security-group-ids sg-bb6718c6
+echo "Ec2 Instances launched sucessfully"
+
+
